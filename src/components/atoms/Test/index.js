302a6e15b1a2f9998bb15d53c5011c45d4e616e1
Steven Whitton <stevenawhitton@gmail.com>
2021-01-06 18:47:06 +0100

    Add Test component and changed file structure

---
+++ src/components/atoms/Test/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import styles from './Test.module.css'
+
+const Test = ({
+  children
+}) => {
+  return ( 
+    <button className={styles.button}>
+      <span>{children}</span>
+    </button>
+   );
+}
+ 
+export default Test;
+
+Test.propTypes = {
+  children: PropTypes.any,
+}
