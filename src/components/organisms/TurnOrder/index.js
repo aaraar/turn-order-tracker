9161d48418813cc1cb10ac5d36c5d2c41aa18ddd
Bas <bdegreeuw@ziggo.nl>
2021-01-11 23:04:39 +0100

    added end_of_line to .editorconfig

--- src/components/organisms/TurnOrder/index.js
+++ src/components/organisms/TurnOrder/index.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import socketIOClient from 'socket.io-client';
 import usernameGenerator from 'username-generator';
 
