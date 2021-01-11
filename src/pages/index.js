15d312886dd84a41157820e8e8f3a7d2a0053826
Bas <bdegreeuw@ziggo.nl>
2021-01-11 23:00:24 +0100

    Updated imports with alias

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head';
-import TurnOrder from '../components/organisms/TurnOrder';
-import styles from '../styles/Home.module.css';
+import TurnOrder from '@/components/organisms/TurnOrder';
+import styles from '@/styles/Home.module.css';
 
 export default function Home() {
   return (
